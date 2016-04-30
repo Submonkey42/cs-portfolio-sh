01abc52eae4899ac49c07f6ac3be83bbcf8f9171
submonkey42 <Submonkey42@gmail.com>
2016-04-30 21:29:59 +0000

    Updated age on info page.

---
+++ arduinoInteract.js
@@ -0,0 +1,22 @@
+// JavaScript File
+var b1= getElementById("arduino_button_1.jpg");
+var b2= getElementById("arduino_button_2.jpg")
+var b1and2= getElementById("arduino_button_1_and_2.jpg")
+
+
+
+
+function button1() {
+    firstbutton = document.getElementById("arduinoImage")
+    firstbutton.src = "arduino_button_1.jpg"
+}
+
+function button2() {
+    secondbutton = document.getElementById("arduinoImage")
+    secondbutton.src = "arduino_button_2.jpg"
+}
+
+function button1and2() {
+    bothbuttons = document.getElementById("arduinoImage")
+    bothbuttons.src = "arduino_button_1_and_2.jpg"
+}
