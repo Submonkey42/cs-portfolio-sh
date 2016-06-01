d8a2ff6e78063e41570f0813181335ce36e00787
submonkey42 <Submonkey42@gmail.com>
2016-06-01 23:02:41 +0000

    Finished Arduino Model page.

--- arduinoInteract.js
+++ arduinoInteract.js
@@ -1,30 +1,50 @@
 // JavaScript File
-var b0= getElementById("arduino_default.jpg")
-var b1= getElementById("arduino_button_1.jpg");
-var b2= getElementById("arduino_button_2.jpg")
-var b1and2= getElementById("arduino_button_1_and_2.jpg")
+
+var arduinoImage = document.getElementById("arduinoImage");
+
+var b1on= false;
+var b2on= false;
+var buzzer= new Audio('buzzer.wav');
 
 
 function button0() {
-    nobutton = document.getElementById("arduinoImage")
-    nobutton.src = "arduino_default.jpg"
+    b2on=false;
+    buzzer.loop=false;
+            if(b1on == false){
+    arduinoImage.src = "arduino_default.jpg";
+        }
+        
+        if(b1on){
+    arduinoImage.src = "arduino_button_1.jpg";
+    }
+        
 }
+
 
 function button1() {
-    firstbutton = document.getElementById("arduinoImage")
-    firstbutton.src = "arduino_button_1.jpg"
-     button1offdiv.style.zIndex = "15";   
-
+    b1on = !b1on;
+    
+        if(b1on == false){
+    arduinoImage.src = "arduino_default.jpg";
+        }
+        
+        if(b1on){
+    arduinoImage.src = "arduino_button_1.jpg";
+    }
+        
 }
+
 
 function button2() {
-    secondbutton = document.getElementById("arduinoImage")
-    secondbutton.src = "arduino_button_2.jpg"
+    arduinoImage.src = "arduino_button_2.jpg";
+    b2on=true;
+        if(b1on&&b2on){
+    arduinoImage.src = "arduino_button_1_and_2.jpg";
+    buzzer.play();
+    buzzer.loop=true;
+    }
 }
 
-function button1and2() {
-    bothbuttons = document.getElementById("arduinoImage")
-    bothbuttons.src = "arduino_button_1_and_2.jpg"
-}
+
 
 
