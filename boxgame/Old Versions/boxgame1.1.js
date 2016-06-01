8f2d0e279c9d0e785553d6b24fb5d40c9ecbf66b
submonkey42 <Submonkey42@gmail.com>
2016-06-01 20:26:40 +0000

    Added link to games page. Improved hardware page.

--- boxgame/Old Versions/boxgame1.1.js
+++ boxgame/Old Versions/boxgame1.1.js
@@ -8,48 +8,12 @@ var ctx = mycanvas.getContext("2d");
 var bullets = [];
 var enemies = [];
 var enemiesSP1 = [];
-var enemiesSP2 = [];
 var BULLETSPD = 3;
 var ENEMYSPD = 2;
 var ENEMYSP1SPD = 4;
-var ENEMYSP2SPD = 2;
-
 var bulletImg = document.getElementById("bulletImg");
 var bombImg = document.getElementById("bombImg");
-
-    var ocean_F1 = document.getElementById("oceanF1");
-    var ocean_F2 = document.getElementById("oceanF2");
-    var ocean_F3 = document.getElementById("oceanF3");
-    var ocean_F4 = document.getElementById("oceanF4");
-    var ocean_F5 = document.getElementById("oceanF5");
-    var ocean_F6 = document.getElementById("oceanF6");
-    var ocean_F7 = document.getElementById("oceanF7");
-    var ocean_F8 = document.getElementById("oceanF8");
-    var ocean_F9 = document.getElementById("oceanF9");
-    var ocean_F10 = document.getElementById("oceanF10");
-    var ocean_F11 = document.getElementById("oceanF11");
-    var ocean_F12 = document.getElementById("oceanF12");
-    var ocean_F13 = document.getElementById("oceanF13");
-    var ocean_F14 = document.getElementById("oceanF14");
-    var ocean_F15 = document.getElementById("oceanF15");
-    var ocean_F16 = document.getElementById("oceanF16");
-    var ocean_F17 = document.getElementById("oceanF17");
-    var ocean_F18 = document.getElementById("oceanF18");
-    var ocean_F19 = document.getElementById("oceanF19");
-    var ocean_F20 = document.getElementById("oceanF20");
-    var ocean_F21 = document.getElementById("oceanF21");
-    var ocean_F22 = document.getElementById("oceanF22");
-    var ocean_F23 = document.getElementById("oceanF23");
-    var ocean_F24 = document.getElementById("oceanF24");
-    var ocean_F25 = document.getElementById("oceanF25");
-    var ocean_F26 = document.getElementById("oceanF26");
-    var ocean_F27 = document.getElementById("oceanF27");
-    var ocean_F28 = document.getElementById("oceanF28");
-    var ocean_F29 = document.getElementById("oceanF29");
-    var ocean_F30 = document.getElementById("oceanF30");
-    var ocean_F31 = document.getElementById("oceanF31");
-    var ocean_F32 = document.getElementById("oceanF32");
-
+var oceanImg = document.getElementById("oceanImg");
 var gameOverImg = document.getElementById("gameOverImg");
 var retryImg = document.getElementById("retryImg");
 var skullImg = document.getElementById("skullImg");
@@ -59,54 +23,10 @@ var bgspd = .5;
 var score = 0;
 var boxAlive = true;
 var EnemySP1_Spawnrate = 0;
-var EnemySP2_Spawnrate = 0;
-var gameRun = true;
-
-//      ANIMATIONS
-//      | | | | |
-//      v v v v v
-var ocean = new Array();
-    ocean[0] = ocean_F1;
-    ocean[1] = ocean_F2;
-    ocean[2] = ocean_F3;
-    ocean[3] = ocean_F4;
-    ocean[4] = ocean_F5;
-    ocean[5] = ocean_F6;
-    ocean[6] = ocean_F7;
-    ocean[7] = ocean_F8;
-    ocean[8] = ocean_F9;
-    ocean[9] = ocean_F10;
-    ocean[10] = ocean_F11;
-    ocean[11] = ocean_F12;
-    ocean[12] = ocean_F13;
-    ocean[13] = ocean_F14;
-    ocean[14] = ocean_F15;
-    ocean[15] = ocean_F16;
-    ocean[16] = ocean_F17;
-    ocean[17] = ocean_F18;
-    ocean[18] = ocean_F19;
-    ocean[19] = ocean_F20;
-    ocean[20] = ocean_F21;
-    ocean[21] = ocean_F22;
-    ocean[22] = ocean_F23;
-    ocean[23] = ocean_F24;
-    ocean[24] = ocean_F25;
-    ocean[25] = ocean_F26;
-    ocean[26] = ocean_F27;
-    ocean[27] = ocean_F28;
-    ocean[28] = ocean_F29;
-    ocean[29] = ocean_F30;
-    ocean[30] = ocean_F31;
-    ocean[31] = ocean_F32;
+var gamePaused = false;
 
 
 
-
-
-
-//      GAME OBJECTS
-//      | | | | | | 
-//      v v v v v v
 
 
 var box = {
@@ -138,14 +58,6 @@ var box = {
            if(this.xPos < enemiesSP1[i].xPos + enemiesSP1[i].width && this.xPos + this.width > enemiesSP1[i].xPos && this.yPos < enemiesSP1[i].yPos + enemiesSP1[i].width && this.yPos + this.width > enemiesSP1[i].yPos && boxAlive) {
                 box.hp = box.hp-100;
                 enemiesSP1.splice(i,1);
-           }
-        }
-        
-        
-        for(i=0;i<enemiesSP2.length;i++) {
-           if(this.xPos < enemiesSP2[i].xPos + enemiesSP2[i].width && this.xPos + this.width > enemiesSP2[i].xPos && this.yPos < enemiesSP2[i].yPos + enemiesSP2[i].width && this.yPos + this.width > enemiesSP2[i].yPos && boxAlive) {
-                box.hp = box.hp-100;
-                enemiesSP2.splice(i,1);
            }
         }
         
@@ -223,32 +135,16 @@ function Bullet(xPos, yPos) {
        this.yPos -= BULLETSPD;
        
        for(i=0;i<enemies.length;i++) {
-            if(this.xPos < enemies[i].xPos + enemies[i].width && this.xPos + this.width > enemies[i].xPos && this.yPos < enemies[i].yPos + enemies[i].width && this.yPos + this.width > enemies[i].yPos) {
-                enemies[i].hp = enemies[i].hp-1;
-                score = score+100;
-            if(enemies[i].hp==0){
+           if(this.xPos < enemies[i].xPos + enemies[i].width && this.xPos + this.width > enemies[i].xPos && this.yPos < enemies[i].yPos + enemies[i].width && this.yPos + this.width > enemies[i].yPos) {
                 enemies.splice(i,1);
-        }
+                score = score+100;
            }
         }
         
         for(i=0;i<enemiesSP1.length;i++) {
-            if(this.xPos < enemiesSP1[i].xPos + enemiesSP1[i].width && this.xPos + this.width > enemiesSP1[i].xPos && this.yPos < enemiesSP1[i].yPos + enemiesSP1[i].width && this.yPos + this.width > enemiesSP1[i].yPos) {
-                enemiesSP1[i].hp = enemiesSP1[i].hp-1;
-                score = score+500;
-            if(enemiesSP1[i].hp==0){
+           if(this.xPos < enemiesSP1[i].xPos + enemiesSP1[i].width && this.xPos + this.width > enemiesSP1[i].xPos && this.yPos < enemiesSP1[i].yPos + enemiesSP1[i].width && this.yPos + this.width > enemiesSP1[i].yPos) {
                 enemiesSP1.splice(i,1);
-        }
-           }
-        }
-        
-        for(i=0;i<enemiesSP2.length;i++) {
-            if(this.xPos < enemiesSP2[i].xPos + enemiesSP2[i].width && this.xPos + this.width > enemiesSP2[i].xPos && this.yPos < enemiesSP2[i].yPos + enemiesSP2[i].width && this.yPos + this.width > enemiesSP2[i].yPos) {
-                enemiesSP2[i].hp = enemiesSP2[i].hp-1;
                 score = score+500;
-            if(enemiesSP2[i].hp==0){
-                enemiesSP2.splice(i,1);
-        }
            }
         }
        
@@ -263,9 +159,7 @@ function Enemy(xPos, yPos) {
     this.yPos = yPos;
     this.width = 20;
     this.height = 20;
-    this.hp = 1;
-    
-
+    this.health = 1;
     
     this.draw = function(){
         ctx.rect(this.xPos, this.yPos, 20, 20);
@@ -288,10 +182,8 @@ function EnemySP1(xPos, yPos) {
     this.yPos = yPos;
     this.width = 18;
     this.height = 18;
-    this.hp = 1;
+    this.health = 1;
     
-    
-
     this.draw = function(){
         ctx.fillStyle="#00FF00";
         ctx.beginPath();
@@ -311,37 +203,7 @@ function EnemySP1(xPos, yPos) {
         }
     };
 }
-
-
     
-function EnemySP2(xPos, yPos) {
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.width = 25;
-    this.height = 25;
-    this.hp = 20;
-    
-    
-    
-    this.draw = function(){
-        ctx.fillStyle="#FF0000";
-        ctx.beginPath();
-        ctx.rect(this.xPos, this.yPos, 25, 25);
-        ctx.fillRect(this.xPos, this.yPos, 25, 25);
-        ctx.stroke();
-        ctx.closePath();
-
-    };
-    this.move = function(){
-        this.yPos +=ENEMYSP2SPD;
-        
-        for(i=0;i<bullets.length;i++) {
-           if(this.xPos < bullets[i].xPos + bullets[i].width && this.xPos + this.width > bullets[i].xPos && this.yPos < bullets[i].yPos + bullets[i].width && this.yPos + this.width > bullets[i].yPos) {
-            //    bullets.splice(i,1);
-           }
-        }
-    };
-}    
 
     
     
@@ -404,10 +266,6 @@ document.addEventListener("keydown", function(evt) {
     ) {
         box.shootDefault = true;
     }
-    if (evt.keyCode === 80 && boxAlive
-    ) {
-        gameRun=false;
-    }
 });
 
 document.addEventListener("keyup", function(evt) {
@@ -462,13 +320,12 @@ function gameLoop() {
 
 
     EnemySP1_Spawnrate = Math.floor((Math.random()*50+1));
-    EnemySP2_Spawnrate = Math.floor((Math.random()*50+1));
 
 
     
     if(bullets.length > 0 && bullets[0]["yPos"] < 0) {
         bullets.shift();   
-    }
+    };
 
     if(enemies.length > 0 && enemies[0].yPos > 500) {
         enemies.shift();
@@ -495,14 +352,8 @@ function gameLoop() {
         enemiesSP1[i].draw();
         
     }
-    
-    for(var i =0; i<enemiesSP2.length; i++){
-        enemiesSP2[i].move();
-        enemiesSP2[i].draw();
-    }
-    if(gameRun){
-        window.requestAnimationFrame(gameLoop);
-    }
+
+    window.requestAnimationFrame(gameLoop);
 }
 gameLoop();
 
@@ -511,15 +362,9 @@ var wave1 = setInterval(function(){
     enemies.push(new Enemy(Math.random() * mycanvas.width-20, 0));
 }, 500);
 
-var waveSP1 = setInterval(function(){
-    if(EnemySP1_Spawnrate==42){
+var waveSP = setInterval(function(){
+    if(EnemySP1_Spawnrate==10){
     enemiesSP1.push(new EnemySP1(Math.random() * mycanvas.width-20, 0));
-    }
-},100);
-
-var waveSP2 = setInterval(function(){
-    if(EnemySP2_Spawnrate==42){
-    enemiesSP2.push(new EnemySP2(Math.random() * mycanvas.width-20, 0));
     }
 },100);
 
@@ -549,3 +394,4 @@ var waveSP2 = setInterval(function(){
 //     }
 
 
+Status 
